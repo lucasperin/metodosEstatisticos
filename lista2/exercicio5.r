dfc15e2c8c9c609cead479469307209dbffa7d06
Lucas Perin <lucas.perin@posgrad.ufsc.br>
2016-05-23 18:08:50 -0300

    improve printing

--- lista2/exercicio5.r
+++ lista2/exercicio5.r
@@ -27,33 +27,26 @@ printSample = function(sample, size, title, originalData) {
 	sp = sqrt(p*(2-p)/n) * sqrt((N-n)/(N-1))
 	print("Intervalo:")
 	print(1.96*sp)
-	message("Para amostra com ", size, " elementos, o intervalo de 95% de confianca para p
-	está entre ", p - 1.96*sp, " e ", p + 1.96*sp)
+	message("Para amostra com ", size, " elementos, o intervalo de 95% de confianca para p está entre ", p - 1.96*sp, " e ", p + 1.96*sp)
 	
 	e0 = 0.02
 	n0 = (((1.96)^2) * p * (1-p))/e0^2
 
-	message("Para que se obtenha nível de confiança de 95% com erro máximo de ", e0,
-	" é necessário uma amostra com no mínimo ", N*n0/(N+n0), " elementos (corrigido).
-	valor de n0 original = ", n0)
+	message("Para que se obtenha nível de confiança de 95% com erro máximo de ", e0," é necessário uma amostra com no mínimo ", N*n0/(N+n0), " elementos (corrigido). valor de n0 original = ", n0)
+print("==============================================")
 }
 
 
 data = readTableWithoutNA()$Opinião
-print("Sumário dos dados originais:")
-summary(data)
+print("==============================================")
+print("=======Sumário dos dados originais============")
+print("==============================================")
 to = table(data)
-freqo1 = to[names(to)=="Insatisfeito"]
-print("Frequência original:")
-print(freqo1)
-freqo2 = to[names(to)=="Muito insatisfeito"]
-print("Frequência original:")
-print(freqo2)
-freqo = freqo1 + freqo2
-print("Proporcao original p:")
-po = freqo/length(data)
-print(po)
+print(to)
+print("proporcoes originais:")
+print(to/length(data))
 
-print("====AMOSTRAS=====")
+print("==============================================")
+print("=================AMOSTRAS=====================")
 
 printSample(samplefy(data, 200), 200, "Amostra de Opiniao com 200 dados", data)
