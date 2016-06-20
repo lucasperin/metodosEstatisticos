62e9d2488b1735b407d1b64bb7c2501298d88475
Lucas Perin <lucas.perin@posgrad.ufsc.br>
2016-06-20 20:33:56 -0300

    remove trash files.. wrong wrong!

---
+++ lista3/Questao2c.R
@@ -0,0 +1,17 @@
+library(pwr)
+
+poder_do_teste = function(delta, sdev){
+  power.t.test(n = 20,d = delta, sd = sdev, sig.level = 0.01, power = NULL, type = "one.sample", alternative = "one.sided", strict = FALSE)
+}
+
+h0 = 4.5
+sdev = 0.77
+
+poder_do_teste(abs(3.50 - h0), sdev)
+poder_do_teste(abs(3.75 - h0), sdev)
+poder_do_teste(abs(3.85 - h0), sdev)
+poder_do_teste(abs(3.95 - h0), sdev)
+poder_do_teste(abs(4.15 - h0), sdev)
+poder_do_teste(abs(4.25 - h0), sdev)
+poder_do_teste(abs(4.35 - h0), sdev)
+
