f3db3e3f27c13d6a5456b6a8a1992af70377ee5e
Lucas Perin <lucas.perin@posgrad.ufsc.br>
2016-06-20 18:56:19 -0300

    finish item b

--- lista3/Questao2b.R
+++ lista3/Questao2b.R
@@ -1,7 +1,7 @@
 library(pwr)
 
 poder_do_teste = function(delta, sdev){
-  power.t.test(n = 20,d = delta, sd = sdev, sig.level = 0.05, power = NULL, type = "one.sample", alternative = "one.sided", strict = FALSE)
+  power.t.test(n = 20,d = delta, sd = sdev, sig.level = 0.01, power = NULL, type = "one.sample", alternative = "one.sided", strict = FALSE)
 }
 
 h0 = 4.5
@@ -13,3 +13,5 @@ poder_do_teste(abs(3.85 - h0), sdev)
 poder_do_teste(abs(3.95 - h0), sdev)
 poder_do_teste(abs(4.15 - h0), sdev)
 poder_do_teste(abs(4.25 - h0), sdev)
+poder_do_teste(abs(4.35 - h0), sdev)
+
