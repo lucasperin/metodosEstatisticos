f463160009a5e0a95dc5c701262cdc3993644f75
Lucas Perin <lucas.perin@posgrad.ufsc.br>
2016-06-20 20:25:52 -0300

    remove wrong files

--- lista3/Questao2c50.R
+++ lista3/Questao2c50.R
@@ -1,5 +1,4 @@
-#le os dados 
-data1 = read.csv(file = "amostraRenda50.csv")
+data = read.csv(file = "amostra3.csv")
 
 #imprime um sumario dos dados
 summary(data$x)
@@ -12,35 +11,39 @@ n = length(data$x)
 
 #hipotese nula
 H0 = 27
+message("H0 = ", 27)
 
 #nivel de significancia
 alpha = 0.05
+message("alpha = ", alpha)
 
 #hipotese a ser testada
 xbar = mean(data$x)
-xbar
+message("Hipotese = ", xbar)
 
 #desvio padrao
 s = sd(data$x)
-s
+message("sd = ", s)
 
 #graus de liberdade t student
 df = n-1
-df
+message("degrees of freedom (n-1) = ", df)
 
-#cálculo de t(n-1) graus de liberdade
+#calculo de t(n-1) graus de liberdade
 t = ((xbar-H0)*sqrt(n))/s 
-t
+message("t(n-1) = ", t)
 
 #valor de probabilidade P(t(n-1)>t) distribuição central
 p1 = pt(t, df, lower.tail = FALSE, log.p = FALSE)
-p1
+message("P(t(n-1) > ", t, ") = ", p1)
+
 
 #valor de probabilidade P(t(n-1)>t) distribuição NAO central
 ncp = (H0-xbar)*sqrt(n)/s
 ncp
 p2 = pt(t, df, ncp, lower.tail = FALSE, log.p = FALSE)
 p2
+message("Non central P(t(n-1) > ", t, ") = ", p2)
 
 if (p1>alpha){print("Aceita-se H0")}else{print("rejeita-se H0")}
 
