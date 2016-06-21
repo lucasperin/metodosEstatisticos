4dded5518e2c465fb90e77a7efbc95cb409f874c
Lucas Perin <lucas.perin@posgrad.ufsc.br>
2016-06-21 15:07:51 -0300

    done

--- lista3/Questao5a.R
+++ lista3/Questao5a.R
@@ -4,16 +4,24 @@ readTableWithoutNA = function(){
 
 
 data = readTableWithoutNA()
+summary(data)
+
 print("Sumário da renda para Incentivos federais")
 renda1 = data[ which(data$PagamentoC == "Incentivos federais"), ]$Renda
 summary(renda1)
+print("Variância para Incentivos federais")
 print(var(renda1))
+print("Desvio Padrão para Incentivos federais")
+print(sd(renda1))
 
 #summary(data[which(data$PagamentoC = "Incentivos federais"),])
 
 print("Sumário da renda para Outras formas de pagamento")
 renda2 = data[ which(data$PagamentoC != "Incentivos federais"), ]$Renda
 summary(renda2)
+print("Variância para Outras formas de pagamento")
 print(var(renda2))
+print("Desvio Padrão para Outras formas de pagamento")
+print(sd(renda2))
 
 
